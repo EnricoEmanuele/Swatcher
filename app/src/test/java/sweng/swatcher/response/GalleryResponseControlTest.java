d14e2b369241436ec1c65dd01a8956cc83a1d015
Wilmer <wilmer.ciasullo@hotmail.com>
2016-10-24 15:24:09 +0200

    #27 adding Unit Test Class

---
+++ app/src/test/java/sweng/swatcher/response/GalleryResponseControlTest.java
@@ -0,0 +1,21 @@
+package sweng.swatcher.response;
+
+import org.junit.Test;
+
+/**
+ * Created by WILMER on 22/10/2016.
+ */
+
+public class GalleryResponseControlTest {
+
+    @Test
+    public void test_GalleryResponseControl(){
+        // Logic to simulate and test Constructor of GalleryResponseControl
+    }
+
+    @Test
+    public void test_checkResponse(){
+        // Logic to test Constructor checkResponse
+    }
+
+}
