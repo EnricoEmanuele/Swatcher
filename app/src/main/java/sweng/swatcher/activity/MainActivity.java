79d6015bee80fe07c61f5de7f221465666630802
Antonio De Simone <antoniods311@gmail.com>
2016-10-26 16:59:22 +0200

    Modified MainActivity.

--- app/src/main/java/sweng/swatcher/activity/MainActivity.java
+++ app/src/main/java/sweng/swatcher/activity/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -39,8 +38,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (!io.vov.vitamio.LibsChecker.checkVitamioLibs(this))
-            return;
 
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
