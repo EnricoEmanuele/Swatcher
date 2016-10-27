ff04a201c37e7c9d3d7116b10bb101ad480e8405
EnricoEmanuele <enricoemanuele@gmail.com>
2016-10-27 12:53:12 +0200

    Update MediaParserTest

--- app/src/test/java/sweng/swatcher/model/MediaParserTest.java
+++ app/src/test/java/sweng/swatcher/model/MediaParserTest.java
@@ -20,7 +20,7 @@ public class MediaParserTest {
     @BeforeClass
     public void setUpJsonArray() throws JSONException {
 
-        //this.inputJsonArray = new JSONArray("[{\"name\":\"02-20161026153311.avi.mp4\",\"extension\":\"mp4\",\"size\":85255,\"date\":\"October 26 2016 15:33:42.\",\"path\":\"picam\/02-20161026153311.avi.mp4\"},{\"name\":\"01-20161026145359.avi.mp4\",\"extension\":\"mp4\",\"size\":145265,\"date\":\"October 26 2016 14:54:31.\",\"path\":\"picam\/01-20161026145359.avi.mp4\"},{\"name\":\"02-20161026153312-01.jpg\",\"extension\":\"jpg\",\"size\":9995,\"date\":\"October 26 2016 15:33:41.\",\"path\":\"picam\/02-20161026153312-01.jpg\"},{\"name\":\"01-20161026145359-05.jpg\",\"extension\":\"jpg\",\"size\":9721,\"date\":\"October 26 2016 14:54:29.\",\"path\":\"picam\/01-20161026145359-05.jpg\"}]");
+        this.inputJsonArray = new JSONArray("[{\"name\":\"02-20161026153311.avi.mp4\",\"extension\":\"mp4\",\"size\":85255,\"date\":\"October 26 2016 15:33:42.\",\"path\":\"picam\\/02-20161026153311.avi.mp4\"},{\"name\":\"01-20161026145359.avi.mp4\",\"extension\":\"mp4\",\"size\":145265,\"date\":\"October 26 2016 14:54:31.\",\"path\":\"picam\\/01-20161026145359.avi.mp4\"},{\"name\":\"02-20161026153312-01.jpg\",\"extension\":\"jpg\",\"size\":9995,\"date\":\"October 26 2016 15:33:41.\",\"path\":\"picam\\/02-20161026153312-01.jpg\"},{\"name\":\"01-20161026145359-05.jpg\",\"extension\":\"jpg\",\"size\":9721,\"date\":\"October 26 2016 14:54:29.\",\"path\":\"picam\\/01-20161026145359-05.jpg\"}]");
 
         /*JSONParser parser = new JSONParser();
 
