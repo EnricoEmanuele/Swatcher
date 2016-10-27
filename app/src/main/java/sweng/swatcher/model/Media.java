814ef68b45d7618aa0fd9a0ad271702473fa4632
Wilmer <wilmer.ciasullo@hotmail.com>
2016-10-27 14:49:48 +0200

    add equals method according to issue #33

--- app/src/main/java/sweng/swatcher/model/Media.java
+++ app/src/main/java/sweng/swatcher/model/Media.java
@@ -55,4 +55,33 @@ public class Media {
     public void setName(String name) {
         this.name = name;
     }
+
+    // equals is null safe but s taken for granted that all proprties
+    // of this Object are always different from null
+    public boolean equals(Media otherMedia){
+        if (otherMedia == null)
+            return false;
+
+        String otherName = otherMedia.getName();
+        String otherExtension = otherMedia.getExtension();
+        String otherDate = otherMedia.getDate();
+        String otherPath = otherMedia.getPath();
+        String otherSize = otherMedia.getSize();
+
+        if( (this.name != null && this.name.equals(otherName))
+                &&
+                (this.extension != null && this.extension.equals(otherExtension))
+                &&
+                (this.date != null && this.date.equals(otherDate))
+                &&
+                (this.path != null && this.path.equals(otherPath))
+                &&
+                (this.size != null && this.size.equals(otherSize))
+                )
+            return true;
+
+        else return false;
+
+    }
+
 }
