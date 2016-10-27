b969b1e662cff61695a99b469a88ad5fade8073b
Antonio De Simone <antoniods311@gmail.com>
2016-10-27 19:35:38 +0200

    Modified MediaSettingFragment class. Closed #34

--- app/src/main/java/sweng/swatcher/fragment/MediaSettingFragment.java
+++ app/src/main/java/sweng/swatcher/fragment/MediaSettingFragment.java
@@ -104,7 +104,6 @@ public class MediaSettingFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
     }
 
     @Override
