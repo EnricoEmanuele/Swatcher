6256366c85b203e7ae0616e0ca492c29cf28d946
Antonio De Simone <antoniods311@gmail.com>
2016-11-08 13:27:17 +0100

    Added GalleryFrafmentTest class.

--- app/src/androidTest/java/sweng/swatcher/activity/GalleryFragmentTest.java
+++ app/src/androidTest/java/sweng/swatcher/activity/GalleryFragmentTest.java
@@ -1,7 +1,5 @@
 package sweng.swatcher.activity;
 
-
-import android.support.test.espresso.DataInteraction;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -10,24 +8,21 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
-
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import sweng.swatcher.R;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
-import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -71,25 +66,75 @@ public class GalleryFragmentTest {
     @Test
     public void galleryFragmentTest() {
 
-        ViewInteraction floatingActionButton2 = onView(
-                allOf(withId(R.id.gallery_button), isDisplayed()));
-        floatingActionButton2.perform(click());
+        ViewInteraction appCompatImageButton3 = onView(
+                allOf(withContentDescription("Open Menu"),
+                        withParent(withId(R.id.toolbar)),
+                        isDisplayed()));
+        appCompatImageButton3.perform(click());
 
-        /*ViewInteraction relativeLayout = onView(
-                allOf(withId(R.id.single_item_gallery),
+        ViewInteraction appCompatCheckedTextView3 = onView(
+                allOf(withId(R.id.design_menu_item_text), withText("Gallery"), isDisplayed()));
+        appCompatCheckedTextView3.perform(click());
+
+        ViewInteraction listView = onView(
+                allOf(withId(R.id.gallery_list_view),
                         childAtPosition(
-                                withId(R.id.gallery_list_view),
+                                allOf(withId(R.id.gallery_rel_lay),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+                                                0)),
                                 0),
                         isDisplayed()));
-        relativeLayout.perform(click());*/
+        listView.check(matches(isDisplayed()));
 
-        /*DataInteraction dataInteraction = onData(anything()).inAdapterView(withId(R.id.gallery_list_view)).atPosition(0);
-        dataInteraction.perform(click());*/
+        ViewInteraction floatingActionButton2 = onView(
+                allOf(withId(R.id.gallery_button),
+                        withParent(withId(R.id.gallery_rel_lay)),
+                        isDisplayed()));
+        floatingActionButton2.perform(click());
 
-        ViewInteraction appCompatButton = onView(
+        ViewInteraction relativeLayout = onView(
+                allOf(withId(R.id.single_item_gallery),
+                        childAtPosition(
+                                allOf(withId(R.id.gallery_list_view),
+                                        childAtPosition(
+                                                withId(R.id.gallery_rel_lay),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        relativeLayout.check(matches(isDisplayed()));
+
+        ViewInteraction imageView = onView(
+                allOf(withId(R.id.image), withContentDescription("Settings"),
+                        childAtPosition(
+                                allOf(withId(R.id.single_item_gallery),
+                                        childAtPosition(
+                                                withId(R.id.gallery_list_view),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        imageView.check(matches(isDisplayed()));
+
+        imageView.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.close_button), withText(" CLOSE ")));
+        appCompatButton2.check(matches(isDisplayed()));
+        appCompatButton2.perform(click());
+
+        /*ViewInteraction button = onView(
+                allOf(withId(R.id.close_button),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
+
+        ViewInteraction appCompatButton3 = onView(
                 allOf(withId(R.id.close_button), withText(" CLOSE "), isDisplayed()));
-        appCompatButton.perform(click());
-
+        appCompatButton3.perform(click());*/
     }
 
     private static Matcher<View> childAtPosition(
