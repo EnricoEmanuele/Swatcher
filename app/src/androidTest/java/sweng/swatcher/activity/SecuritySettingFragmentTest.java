93205a68f6df04518aadbe80fd6bf02f9d875ff6
Antonio De Simone <antoniods311@gmail.com>
2016-11-09 17:07:25 +0100

    Modified SecuritySettingFragmentTest class. Added new string value in strings.xml

--- app/src/androidTest/java/sweng/swatcher/activity/SecuritySettingFragmentTest.java
+++ app/src/androidTest/java/sweng/swatcher/activity/SecuritySettingFragmentTest.java
@@ -1,26 +1,23 @@
 package sweng.swatcher.activity;
 
-
+import android.support.annotation.StringRes;
 import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
-
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import sweng.swatcher.R;
-
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -29,6 +26,7 @@ import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -41,10 +39,12 @@ public class SecuritySettingFragmentTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
     private static ConnectionSettingFragmentTest connSettingTest;
+    private static String newUsername, newPassword;
 
     @BeforeClass
     public static void setUp(){
         connSettingTest = new ConnectionSettingFragmentTest();
+        setNewCredentials();
     }
 
     @Before
@@ -56,48 +56,72 @@ public class SecuritySettingFragmentTest {
     @Test
     public void securitySettingFragmentTest() {
 
-        ViewInteraction appCompatImageButton2 = onView(
+        ViewInteraction openMenu = onView(
                 allOf(withContentDescription("Open Menu"),
                         withParent(withId(R.id.toolbar)),
                         isDisplayed()));
-        appCompatImageButton2.perform(click());
+        openMenu.perform(click());
 
-        ViewInteraction appCompatCheckedTextView2 = onView(
+        ViewInteraction openSecuritySettings = onView(
                 allOf(withId(R.id.design_menu_item_text), withText("Security Settings"), isDisplayed()));
-        appCompatCheckedTextView2.perform(click());
+        openSecuritySettings.perform(click());
 
-        ViewInteraction appCompatEditText14 = onView(
+        //change credentials
+        changeUsername(newUsername);
+        changePassword(newPassword);
+
+        try{
+            Thread.sleep(10000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //click change button and wait for result
+        ViewInteraction changeButton = onView(
+                allOf(withId(R.id.change_button), isDisplayed()));
+        changeButton.perform(click());
+        checkSnackBarDisplayedByMessage(R.string.new_credentials_test_message); //snackbar assertion
+
+        /*//reset credentials
+        openMenu.perform(click());
+        openSecuritySettings.perform(click());
+        changeUsername(oldUsername);
+        changePassword(oldPassword);
+
+        try{
+            Thread.sleep(10000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        changeButton.perform(click());
+        checkSnackBarDisplayedByMessage(R.string.old_credentials_test_message); //snackbar assertion
+        */
+
+    }
+
+    private void checkSnackBarDisplayedByMessage(@StringRes int message) {
+        onView(withText(message))
+                .check(matches(withEffectiveVisibility(
+                        ViewMatchers.Visibility.VISIBLE
+                )));
+    }
+
+    private void changeUsername(String value){
+        ViewInteraction appCompatEditText11 = onView(
                 allOf(withId(R.id.new_username), isDisplayed()));
-        appCompatEditText14.perform(replaceText("user"), closeSoftKeyboard());
+        appCompatEditText11.perform(replaceText(value), closeSoftKeyboard());
+    }
 
-        ViewInteraction appCompatEditText15 = onView(
+    private void changePassword(String value){
+        ViewInteraction appCompatEditText12 = onView(
                 allOf(withId(R.id.new_password), isDisplayed()));
-        appCompatEditText15.perform(replaceText("pass"), closeSoftKeyboard());
+        appCompatEditText12.perform(replaceText(value), closeSoftKeyboard());
+    }
 
-        ViewInteraction appCompatEditText16 = onView(
-                allOf(withId(R.id.new_password), withText("pass"), isDisplayed()));
-        appCompatEditText16.perform(pressImeActionButton());
-
-        ViewInteraction editText = onView(
-                allOf(withId(R.id.new_username), withText("user"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
-                                        0),
-                                1),
-                        isDisplayed()));
-        editText.check(matches(withText("user")));
-
-        ViewInteraction editText2 = onView(
-                allOf(withId(R.id.new_password), withText("pass"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
-                                        0),
-                                3),
-                        isDisplayed()));
-        editText2.check(matches(withText("pass")));
-
+    private static void setNewCredentials(){ //select current credentials for test
+        newUsername = "u";
+        newPassword = "p";
     }
 
     private static Matcher<View> childAtPosition(
